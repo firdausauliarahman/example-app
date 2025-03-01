a48c72cae8c8e0cdbf4531262471fa7529862d14
Firdaus Aulia Rahman <firdausauliarahman@gmail.com>
2025-03-01 12:03:10 +0700

    Edit

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Your IP Address : " + clientIP))
+	w.Write([]byte("Your IP Address is : " + clientIP))
 }
 
 func getPublicIP() (string, error) {
